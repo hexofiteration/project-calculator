f9d26cd0f2bcb77e84c36aa4e64334bf355913ba
hexofiteration <hexofiteration@gmail.com>
2026-06-23 16:07:10 +0200

    add basic arithmetic operations with operate()
    Implement add, subtract, multiply, and divide functions, Add operate()
    that takes a symbol (+, -, *, /) and runs the right math function

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){ 
+    return num1 * num2;
+}
+function divide(num1, num2){
+    if (num2 === 0) return "🙄 ugh! Just don't!";
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+
+	if (operator == "+" ) return add(num1, num2);
+
+	if (operator == "-" ) return subtract(num1, num2);
+
+	if (operator == "*") return multiply(num1, num2);
+
+	if (operator == "/" ) return divide(num1, num2);
+}
+
+console.log(operate("+", 2, 5));
+console.log(operate("-", 2, 5));
+console.log(operate("*", 2, 5));
+console.log(operate("*", 0, 5));
+console.log(operate("/", 2, 0));
+console.log(operate("/", 0, 5));
