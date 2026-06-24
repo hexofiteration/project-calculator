3ed66d8b31007c1b05306652ac31bca309486bfb
hexofiteration <hexofiteration@gmail.com>
2026-06-24 17:27:21 +0200

    make number buttons work when clicked
    Add a click listener to each button so clicking it adds that number to
    the left operand and the left operand to the console for testing

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
+let leftOperand = "";
+let rightOperand = "";
+let operator = "";
+const numbers = document.querySelector(".numbers");
+const numBtns = Array.from(numbers.querySelectorAll("button"));
+
+
+numBtns.forEach(button => button.addEventListener("click", () => {
+    leftOperand += button.textContent;
+    console.log(leftOperand);
+}));
+
 function add(num1, num2){
     return num1 + num2;
 }
+
 function subtract(num1, num2){
     return num1 - num2;
 }
+
 function multiply(num1, num2){ 
     return num1 * num2;
 }
+
 function divide(num1, num2){
     if (num2 === 0) return "🙄 ugh! Just don't!";
     return num1 / num2;
 }
 
 function operate(operator, num1, num2){
+
     num1 = Number(num1);
     num2 = Number(num2);
+
 	if (operator == "+" ) return add(num1, num2);
 
 	if (operator == "-" ) return subtract(num1, num2);
@@ -23,10 +40,3 @@ function operate(operator, num1, num2){
 
 	if (operator == "/" ) return divide(num1, num2);
 }
-
-console.log(operate("+", "2", 5));
-console.log(operate("-", 2, 5));
-console.log(operate("*", 2, 5));
-console.log(operate("*", 0, 5));
-console.log(operate("/", 2, 0));
-console.log(operate("/", 0, 5));
