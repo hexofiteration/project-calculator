7c4029e301d0cdfe7054e37f556159f4d5c1f4fc
hexofiteration <hexofiteration@gmail.com>
2026-06-24 10:37:24 +0200

    add state variables and fix string number inputs
    variables to keep track of the left operand, right operand, and operator
    Convert inputs to numbers inside operate()

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ function divide(num1, num2){
 }
 
 function operate(operator, num1, num2){
-
+    num1 = Number(num1);
+    num2 = Number(num2);
 	if (operator == "+" ) return add(num1, num2);
 
 	if (operator == "-" ) return subtract(num1, num2);
@@ -23,7 +24,7 @@ function operate(operator, num1, num2){
 	if (operator == "/" ) return divide(num1, num2);
 }
 
-console.log(operate("+", 2, 5));
+console.log(operate("+", "2", 5));
 console.log(operate("-", 2, 5));
 console.log(operate("*", 2, 5));
 console.log(operate("*", 0, 5));
